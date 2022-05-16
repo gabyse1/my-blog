f9697467ac372de4d6de945dc2ea0f93c4d3d9f1
gabyse1 <gaby.es.iaa@gmail.com>
2022-05-16 12:08:50 -0500

    Set the default render view for the Users and Posts controllers actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,9 @@
 class UsersController < ApplicationController
   def index
-    render html: 'Here is a list of users.'
+    # render html: 'Here is a list of users.'
   end
 
   def show
-    render html: 'Here is the details for a given user.'
+    # render html: 'Here is the details for a given user.'
   end
 end
