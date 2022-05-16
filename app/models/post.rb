42d90529352b66f159f9497e26c359c40082b9d5
gabyse1 <gaby.es.iaa@gmail.com>
2022-05-16 18:45:53 -0500

    Create the method most_recent_comments that returns the five most recent comments for a given post

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,4 +2,8 @@ class Post < ApplicationRecord
   belongs_to :user, class_name: 'User', foreign_key: 'user_id', inverse_of: :posts
   has_many :comments, class_name: 'Comment', foreign_key: 'post_id', inverse_of: :post, dependent: :destroy
   has_many :likes, class_name: 'Like', foreign_key: 'post_id', inverse_of: :post, dependent: :destroy
+
+  def most_recent_comments(items = 5)
+    comments.order(created_at: :desc).take(items)
+  end
 end
