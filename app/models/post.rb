10faecd7f7852f3ddb31162f3c3d28541b72d248
gabyse1 <gaby.es.iaa@gmail.com>
2022-05-17 19:37:28 -0500

    Add validations for the Post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -3,6 +3,11 @@ class Post < ApplicationRecord
   has_many :comments, class_name: 'Comment', foreign_key: 'post_id', inverse_of: :post, dependent: :destroy
   has_many :likes, class_name: 'Like', foreign_key: 'post_id', inverse_of: :post, dependent: :destroy
 
+  validates :title, presence: true, length: { maximum: 250 }
+  validates :text, presence: true
+  validates :commentsCounter, presence: true, numericality: { only_integer: true }, comparison: { greater_than_or_equal_to: 0 }
+  validates :likesCounter, presence: true, numericality: { only_integer: true }, comparison: { greater_than_or_equal_to: 0 }
+
   after_create :increment_posts_counter
   after_destroy :decrement_posts_counter
 
