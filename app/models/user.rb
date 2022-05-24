8b1529eeb766f4378c7edf18bce4490881a1ad38
gabyse1 <gaby.es.iaa@gmail.com>
2022-05-24 17:33:05 -0500

    Create method to setup default values for user sign up in User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,17 +1,18 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  # :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :validatable
+         :recoverable, :rememberable, :validatable,
+         :confirmable
   has_many :posts, class_name: 'Post', foreign_key: 'user_id', inverse_of: :user, dependent: :destroy
   has_many :comments, class_name: 'Comment', foreign_key: 'user_id', inverse_of: :user, dependent: :destroy
   has_many :likes, class_name: 'Like', foreign_key: 'user_id', inverse_of: :user, dependent: :destroy
 
   validates :name, presence: true
-  validates :photo, presence: true
-  validates :bio, presence: true
   validates :postsCounter, presence: true, numericality: { only_integer: true },
                            comparison: { greater_than_or_equal_to: 0 }
+
+  after_initialize :default_values
 
   def most_recent_posts(items = 3)
     posts.order(created_at: :desc).take(items)
@@ -19,5 +20,14 @@ class User < ApplicationRecord
 
   def posts_desc_order
     posts.order(created_at: :desc)
+  end
+
+  private
+
+  def default_values
+    return unless new_record?
+
+    self.photo ||= 'user-profile.png'
+    self.postsCounter ||= 0
   end
 end
