69709e93e5a491d692a5d6065113e0fd471a5082
gabyse1 <gaby.es.iaa@gmail.com>
2022-05-28 09:44:45 -0500

    Update the API routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,4 @@
 Rails.application.routes.draw do
-  scope :api, defaults: {format: :json } do
-      devise_for :users, as: 'api', controllers: { sessions: 'api/sessions', registrations: 'api/registrations' }
-
-      resource :user, only: [:show, :update]
-
-      resources :users, only: [:index, :show] do
-        resources :posts, only: [:index, :show] do
-          resources :comments, only: [:index, :create, :show]
-        end
-      end
-  end
-
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   resources :users, only: %i[index show] do
@@ -20,6 +8,18 @@ Rails.application.routes.draw do
   resources :posts, only: %i[new create destroy] do
     resources :comments, only: %i[index new create destroy]
     resources :likes, only: %i[create destroy]
+  end
+
+  namespace :api do
+    devise_for :users, as: 'api', controllers: { sessions: 'api/sessions', registrations: 'api/registrations' }
+
+    resources :users, only: %i[] do
+      resources :posts, only: %i[index]
+    end
+  
+    resources :posts, only: %i[] do
+      resources :comments, only: %i[index create]
+    end
   end
 
   # Defines the root path route ("/")
